8ba261d2954b783d608f1a18d24be9f69d0adda9
anastasia.gulitsyna <anastasia.gulitsyna@rakuten.de>
2020-03-09 11:59:15 +0100

    added negative tests to checks not allowed characters in login field (<>/ and space)

--- src/test/java/tests/CreateUserNegativeTests.java
+++ src/test/java/tests/CreateUserNegativeTests.java
@@ -5,6 +5,10 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class CreateUserNegativeTests extends BaseTest {
     @BeforeClass
@@ -16,6 +20,8 @@ public class CreateUserNegativeTests extends BaseTest {
     private final String MISSING_LOGIN_MSG = "Login is required!";
     private final String DIFF_PASSWORDS_MSG = "Password doesn't match!";
     private final String DUPLICATE_USERLOGIN_MSG = "Value should be unique: login";
+    private final String SPACE_IN_LOGIN_MSG = "Restricted character ' ' in the name";
+    private final String RESTRICTED_LOGINCHARS_MSG = "login shouldn't contain characters \"<\", \"/\", \">\": login";
 
     @DataProvider
     public Object[] provideUserWithMandatoryFields() {
@@ -25,6 +31,30 @@ public class CreateUserNegativeTests extends BaseTest {
         user.setRepeatPassword("test");
         User[] users = {user};
         return users;
+    }
+
+    @DataProvider
+    //the only restricted field on special symbols is login field
+    //the only not allowed symbols in login name: <>/ and space
+    public Iterator<Object[]> provideUserWithNotAllowedLoginChars() {
+        List<User> users = new ArrayList<>();
+        //user with "/"
+        User userWithSlash = new User();
+        userWithSlash.setLogin("with/");
+        users.add(userWithSlash);
+        //user with "<"
+        User userWithLeftAngleBracket = new User();
+        userWithLeftAngleBracket.setLogin("with<");
+        users.add(userWithLeftAngleBracket);
+        //user with ">"
+        User userWithRightAngleBracket = new User();
+        userWithRightAngleBracket.setLogin("with>");
+        users.add(userWithRightAngleBracket);
+        for (User user: users) {
+            user.setPassword("test");
+            user.setRepeatPassword("test");
+        }
+        return users.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
     }
 
     //tests for not filled in mandatory fields
@@ -67,6 +97,25 @@ public class CreateUserNegativeTests extends BaseTest {
         app.newUserForm.submitUserCreation();
         String actualErrorMessage = app.newUserForm.getErrorMessageOnMandatoryFields();
         Assert.assertEquals(actualErrorMessage, DIFF_PASSWORDS_MSG, "error message doesn't match!");
+    }
+
+    @Test(dataProvider = "provideUserWithMandatoryFields")
+    public void createUserWithSpaceInLogin(User user){
+        app.usersPage.initNewUserCreation();
+        user.setLogin("with space");
+        app.newUserForm.fillInUserCreationForm(user,false);
+        app.newUserForm.submitUserCreation();
+        String actualErrorMessage = app.usersPage.getPopupErrorMessage();
+        Assert.assertEquals(actualErrorMessage, SPACE_IN_LOGIN_MSG, "error message doesn't match!");
+    }
+
+    @Test(dataProvider = "provideUserWithNotAllowedLoginChars")
+    public void provideUserWithNotAllowedLoginChars(User user){
+        app.usersPage.initNewUserCreation();
+        app.newUserForm.fillInUserCreationForm(user,false);
+        app.newUserForm.submitUserCreation();
+        String actualErrorMessage = app.usersPage.getPopupErrorMessage();
+        Assert.assertEquals(actualErrorMessage, RESTRICTED_LOGINCHARS_MSG, "error message doesn't match!");
     }
 
     //duplicated user name
