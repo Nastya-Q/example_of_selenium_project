569ee751a211dfaee5ff2399174da8d243fcba34
anastasia.gulitsyna <anastasia.gulitsyna@rakuten.de>
2020-05-27 16:34:06 +0200

    moved logging functionality to test listener

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,19 +2,15 @@ package tests;
 
 import application.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testng.ITestContext;
-import org.testng.annotations.*;
-
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Listeners;
 import java.io.IOException;
-import java.lang.reflect.Method;
-import java.util.Arrays;
 
 @Listeners(TestListener.class)
 public class BaseTest {
     protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
-    Logger logger = LoggerFactory.getLogger(BaseTest.class);
 
     @BeforeSuite
     public void start(ITestContext context) throws IOException {
@@ -25,15 +21,5 @@ public class BaseTest {
     @AfterSuite(alwaysRun = true)
     public void stop() {
         app.quit();
-    }
-
-    @BeforeMethod
-    public void logTestStart(Method m, Object[] p) {
-        logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
-    }
-
-    @AfterMethod(alwaysRun = true)
-    public void logTestStop(Method m) {
-        logger.info("Stop test " + m.getName());
     }
 }
--- src/test/java/tests/TestListener.java
+++ src/test/java/tests/TestListener.java
@@ -1,30 +1,36 @@
 package tests;
 
 import application.ApplicationManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import java.util.Arrays;
 
 public class TestListener implements ITestListener {
-    @Override
-    public void onTestStart(ITestResult iTestResult) {
+    Logger logger = LoggerFactory.getLogger(BaseTest.class);
 
+    @Override
+    public void onTestStart(ITestResult result) {
+        logger.info("Start test " + result.getName() + " with parameters " + Arrays.asList(result.getParameters()));
     }
 
     @Override
-    public void onTestSuccess(ITestResult iTestResult) {
-
+    public void onTestSuccess(ITestResult result) {
+        logger.info("Test passed " + result.getName() + " with parameters " + Arrays.asList(result.getParameters()));
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
+        logger.info("Test failed " + result.getName() + " with parameters " + Arrays.asList(result.getParameters()));
         ApplicationManager app = (ApplicationManager) result.getTestContext().getAttribute("app");
         app.takeScreenshot(result.getName());
     }
 
     @Override
-    public void onTestSkipped(ITestResult iTestResult) {
-
+    public void onTestSkipped(ITestResult result) {
+        logger.info("Test skipped " + result.getName() + " with parameters " + Arrays.asList(result.getParameters()));
     }
 
     @Override
